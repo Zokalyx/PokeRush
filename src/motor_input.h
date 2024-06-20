267b2b8da4125000e8c4c6e809e1d8951b6f3dcd
Zokalyx <zokalyx@gmail.com>
2024-06-20 20:17:26 -0300

    compatibilidad windows

--- src/motor_input.h
+++ src/motor_input.h
@@ -1,12 +1,23 @@
 #ifndef __INPUT_H__
 #define __INPUT_H__
 
+#ifdef _WIN32
+#define LINEFEED 13
+#define BACKSPACE 8
+#define FLECHA_ARRIBA -72
+#define FLECHA_IZQUIERDA -75
+#define FLECHA_DERECHA -77
+#define FLECHA_ABAJO -80
+#endif
+
+#ifdef __unix__
 #define LINEFEED 10
 #define BACKSPACE 127
 #define FLECHA_ARRIBA -'A'
 #define FLECHA_ABAJO -'B'
 #define FLECHA_DERECHA -'C'
 #define FLECHA_IZQUIERDA -'D'
+#endif
 
 /**
  * Consume y devuelve el primer caracter
@@ -19,18 +30,52 @@
 int leer_caracter();
 
 /**
- * Reestablece la terminal a su estado canónico,
- * con ECHO habilitado.
- */
+ * Habilita la visibilidad de caracteres
+ * tipados (`stdin`) en la terminal.
+*/
 void habilitar_echo_terminal();
 
 /**
- * Deshabilita el ECHO de caracteres
- * en la terminal y el modo canónico.
- * De esta manear no hay Line Buffering
- * y los caracteres pasan directamente
- * a stdin para ser leídos por el programa.
- */
+ * Deshabilita la visibilidad de caracteres
+ * tipados (`stdin`) en la terminal.
+*/
 void deshabilitar_echo_terminal();
 
 #endif // __INPUT_H__
+
+// #ifndef __INPUT_H__
+// #define __INPUT_H__
+
+// #define LINEFEED 10
+// #define BACKSPACE 127
+// #define FLECHA_ARRIBA -'A'
+// #define FLECHA_ABAJO -'B'
+// #define FLECHA_DERECHA -'C'
+// #define FLECHA_IZQUIERDA -'D'
+
+// /**
+//  * Consume y devuelve el primer caracter
+//  * presente en `stdin`, o 0 si no hubiera ninguno.
+//  * No bloquea el programa.
+//  *
+//  * Los caracteres que consisten en secuencias de escape
+//  * se devuelven con valor negativo.
+// */
+// int leer_caracter();
+
+// /**
+//  * Reestablece la terminal a su estado canónico,
+//  * con ECHO habilitado.
+//  */
+// void habilitar_echo_terminal();
+
+// /**
+//  * Deshabilita el ECHO de caracteres
+//  * en la terminal y el modo canónico.
+//  * De esta manear no hay Line Buffering
+//  * y los caracteres pasan directamente
+//  * a stdin para ser leídos por el programa.
+//  */
+// void deshabilitar_echo_terminal();
+
+// #endif // __INPUT_H__
